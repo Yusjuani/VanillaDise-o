201fd5a65b249556c7dedb85f2c4329aafb7d7e6
yusjuani <veroyusjuani@gmail.com>
2024-06-18 18:52:11 -0400

    detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./componentes/Header";
 import Main from "./componentes/Main";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
   return (
     <div className="App">
       <Header />
